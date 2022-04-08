723c4dea8401628f5819c1b994902517afe5bb43
fernando.rezende <luis.santos@tuntscorp.com>
2022-04-08 16:03:23 -0300

    one more change

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,20 +1,10 @@
 import express from "express";
-import * as db from './src/config/db/mockedData.js';
+import { createMockedData } from './src/config/db/mockedData.js';
 import userRoutes from "./src/modules/user/routes/UserRoutes.js"
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 const CONTAINER_ENV = "container";
-
-app.use(express.json());
-
-
-startApplication();
-function startApplication() {
-    if(env.NODE_ENV !== CONTAINER_ENV) {
-        db.createMockedData();
-    }
-}
 
 app.get("/auth", (req, res) => {
     return res.status(200).json({
@@ -24,8 +14,18 @@ app.get("/auth", (req, res) => {
    });
 });
 
+app.use(express.json());
+
+
+startApplication();
+function startApplication() {
+    if(env.NODE_ENV !== CONTAINER_ENV) {
+        createMockedData();
+    }
+}
+
 app.get("/auth/create/mocked-data", (req, res) => {
-    db.createMockedData();
+    createMockedData();
     return res.json({message: "data created"})
 })
 
